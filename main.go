0d0e659de77ad06e59927a5f79f836f956c27d1c
Risman Kurniadi <rismankurniadi2@gmail.com>
2022-10-11 13:18:30 +0700

    membuat struct untuk migrasi ke database

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
@@ -9,6 +10,26 @@ import (
 
 var db *gorm.DB
 var err error
+
+// struct untuk table auto migrasi
+type Orders struct {
+	Order_id      int       `from:"order_id" json:"order_id"`
+	Customer_name string    `from:"customer_name" json:"customer_name"`
+	Ordered_at    time.Time `from:"ordered_at" json:"ordered_at"`
+}
+
+type Items struct {
+	Item_id     int    `from:"item_id" json:"item_id"`
+	Item_code   string `from:"item_code" json:"item_code"`
+	Description string `from:"description" json:"description"`
+	Quantity    int    `from:"quantity" json:"quantity"`
+	Order_id    int    `from:"order_id" json:"order_id"`
+}
+
+type Result struct {
+	Data    interface{} `json:"data"`
+	Message string      `json:"message"`
+}
 
 func main() {
 	//koneksi ke databse
@@ -19,4 +40,6 @@ func main() {
 	} else {
 		log.Panicln("Koeneksi Berhasil")
 	}
+	//auto migrasi database
+	db.AutoMigrate(&Orders{}, &Items{})
 }
